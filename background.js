5d5fd963430dd348c969142a6a6c303b798d04d8
nartco <sebastien.thuillier@hotmail.com>
2021-02-25 21:25:49 +0100

    remove useless link in manifest

--- background.js
+++ background.js
@@ -1,19 +1,18 @@
 let button1 = document.getElementById("activate");
 let button2 = document.getElementById("stop");
 button1.addEventListener("click", async () => {
-//   chrome.webNavigation.onHistoryStateUpdated.addListener(function(details) {
-//     chrome.tabs.onUpdated.addListener(function(tabId, info, tab) {
-//       document.addEventListener("pjax:end", () => {
-//         if (info.status != "complete") {
-//           return;
-//         } else {
-//           chrome.tabs.insertCSS(tabId, { file: "./styles.css" });
-//         }
-//       });
-//     });
-//   }); 
-// try to add extension on new created tab
-
+  //   chrome.webNavigation.onHistoryStateUpdated.addListener(function(details) {
+  //     chrome.tabs.onUpdated.addListener(function(tabId, info, tab) {
+  //       document.addEventListener("pjax:end", () => {
+  //         if (info.status != "complete") {
+  //           return;
+  //         } else {
+  //           chrome.tabs.insertCSS(tabId, { file: "./styles.css" });
+  //         }
+  //       });
+  //     });
+  //   });
+  // try to add extension on new created tab
 
   chrome.tabs.query({}, tab => {
     console.log(tab);
